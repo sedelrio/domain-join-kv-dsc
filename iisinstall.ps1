b9b8e0d9bd4fca647b2f167be03af32caa6fd16a
Sebastian Del Rio <sebastid@microsoft.com>
2017-10-03 14:27:50 -0300

    first sync

---
+++ iisinstall.ps1
@@ -0,0 +1,23 @@
+Configuration iisinstall
+{
+
+Param ( [string] $nodeName )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration -ModuleVersion "1.1"
+
+Node $nodeName
+  {
+
+    WindowsFeature WebServerRole
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }     
+  }
+}
+
+# Compile the configuration file to a MOF format
+IISInstall
+    
+ # Run the configuration on localhost
+ #Start-DscConfiguration -Path .\IISInstall -Wait -Force -Verbose
