43a00e341619086ff4a0d653f01c113c1dad1755
Sebastian Del Rio <sebastid@microsoft.com>
2017-10-03 14:31:22 -0300

    Modules URL modification

--- DomainJoinTemplate.ps1
+++ DomainJoinTemplate.ps1
@@ -1,30 +1,9 @@
-﻿#New-AzureRmResourceGroupDeployment -Name TestingDeploy  -ResourceGroupName "testdomainjoin" -TemplateFile C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\VM - Domain Join.json" -TemplateParameterFile C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\domainjoinparameters.json"
+﻿
 
-
-#create Azure Key Vault
-$vaultname = "kv-sebastid"
-$password = "Passw0rd23456"
-
-New-AzureRmKeyVault -VaultName $vaultname -ResourceGroupName security-sebastid -Location "eastus" -EnabledForTemplateDeployment
-$secretvalue = ConvertTo-SecureString $password -AsPlainText -Force
-Set-AzureKeyVaultSecret -VaultName $vaultname -Name "examplesecret" -SecretValue $secretvalue
-
-
-# Using Secure String for password
-$template = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\VM - Domain Join.json"
-$parameters = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\domainjoinparamsKeyVault.json"
-New-AzureRmResourceGroupDeployment -Name DomainJoin  -ResourceGroupName "testdomainjoin" -TemplateFile $template -TemplateParameterFile $parameters
-
-
-
-# Using Azure Key Vault
-$template = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\VM - Domain Join.KV.json"
-$parameters = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\domainjoinparamsKeyVault.json"
-New-AzureRmResourceGroupDeployment -Name DomainJoin  -ResourceGroupName "testdomainjoin" -TemplateFile $template -TemplateParameterFile $parameters
 
 # Using Azure Key Vault and DSC
-$template = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\VM - Domain Join.KV.DSC.json"
-$parameters = "C:\Users\sebastid\Documents\Documentation\Templates\Domain Join\domainjoinparamsKeyVault.DSC.json"
+$template = "https://raw.githubusercontent.com/sedelrio/domain-join-kv-dsc/master/domainjoin-kv-dsc-parameters.json"
+$parameters = "https://raw.githubusercontent.com/sedelrio/domain-join-kv-dsc/master/domainjoin-kv-dsc-parameters.json"
 New-AzureRmResourceGroupDeployment -Name DomainJoin  -ResourceGroupName "testdomainjoin" -TemplateFile $template -TemplateParameterFile $parameters
 
 
